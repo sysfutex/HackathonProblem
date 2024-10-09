fed1c9ca679989da6beb0f114238f47c35ab8008
Mikhail <sysfutex@gmail.com>
2024-10-09 20:49:38 +0700

    Beautify the code a bit

--- src/HackathonProblem/Service/Hr/Manager/HrManager.cs
+++ src/HackathonProblem/Service/Hr/Manager/HrManager.cs
@@ -5,8 +5,8 @@ namespace HackathonProblem.Service.Hr.Manager;
 public class HrManager(ITeamBuildingStrategy strategy) : IHrManager
 {
     public IEnumerable<Team> BuildTeams(
-        IEnumerable<Employee> teamLeads, IEnumerable<Employee> juniors, IEnumerable<Wishlist> teamLeadsWishlists,
-        IEnumerable<Wishlist> juniorsWishlists
+        IEnumerable<Employee> teamLeads, IEnumerable<Employee> juniors,
+        IEnumerable<Wishlist> teamLeadsWishlists, IEnumerable<Wishlist> juniorsWishlists
     )
     {
         return strategy.BuildTeams(teamLeads, juniors, teamLeadsWishlists, juniorsWishlists);
